98614610b5ce4999032aabc55e35f5e569664df0
Misha Won <152383167+misha-won@users.noreply.github.com>
2025-04-24 17:22:23 +0900

    update excel_write import

--- 250424_excel_write.py
+++ 250424_excel_write.py
@@ -1,13 +1,12 @@
 import os
-import pandas as pd
-from openpyxl import load_workbook
+import openpyxl
 
 base_directory = "C:/Users/user/Desktop/Workspace/교육훈련/폭력예방교육"
 file_name = "2025년 폭력예방교육 이수명단(시립도서관).xlsx"
 file_path = os.path.join(base_directory, file_name)
 
 # 엑셀 파일 로드
-wb = load_workbook(file_path)
+wb = openpyxl.load_workbook(file_path)
 
 for row in wb[0].iter_rows(min_row=3):  # 4번째 행부터 시작
     temp = str(row[4].value)  # 5번째 열 데이터 선택
